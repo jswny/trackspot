959462185a8c3c4bece1b0bf56aff2fe6186ccba
Joe Sweeney <joe@j1.io>
2018-03-01 14:19:38 -0500

    Initial Django setup

--- bigbinarybrand/urls.py
+++ bigbinarybrand/urls.py
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import include
+
+urlpatterns += [
+    path('trackspot/', include('trackspot.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/trackspot/')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
