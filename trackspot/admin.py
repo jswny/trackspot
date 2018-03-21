b116e8f036dde0d3a6740f8b0754f216f7b7405f
Joe Sweeney <joe@j1.io>
2018-03-20 20:24:05 -0400

    Register all models in admin site

--- trackspot/admin.py
+++ trackspot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Genre)
+admin.site.register(Review)
+admin.site.register(User)
+admin.site.register(Critic)
