dcef020c3eb0db9e387e80c0b09e81e2c6c0fb25
cscigliano <36458332+cscigliano@users.noreply.github.com>
2018-03-28 16:49:13 -0400

    final views and template code

--- trackspot/views.py
+++ trackspot/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db.models import Count
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -75,13 +76,35 @@ def song(request, **kwargs):
     song_id = kwargs['pk']
     song = Song.objects.get(pk=song_id)
     album_songs = Song.objects.filter(album__id = song.album.id)
-    song_reviews = Review.objects.filter(song__id = song.id)
+    review_user_rating_perfect = 100
+    song_reviews_users = Review.objects.filter(song__id=song.id).filter(user__critic=None)
+    song_reviews_critics = Review.objects.filter(song__id=song.id).exclude(user__critic=None)
+	
+	
+    review_user_count_critic = Review.objects.filter(song=song_id).exclude(user__critic=None).count()
+    review_user_rating_total_critic = Review.objects.filter(song=song_id).exclude(user__critic=None).aggregate(Sum('rating'))['rating__sum']
+    if(review_user_count_critic != 0):
+        review_user_rating_average_critic = int(round(review_user_rating_total_critic / review_user_count_critic))
+    else:
+        review_user_rating_average_critic = 'No Reviews'
+	
+    review_user_count_user = Review.objects.filter(song=song_id).filter(user__critic=None).count()
+    review_user_rating_total_user = Review.objects.filter(song=song_id).filter(user__critic=None).aggregate(Sum('rating'))['rating__sum']
+    if(review_user_count_user != 0):
+        review_user_rating_average_user = int(round(review_user_rating_total_user / review_user_count_user))
+    else:
+	    review_user_rating_average_user = 'No Reviews'
+	
     return render(request, 
 	'trackspot/song.html',
 	context = {
 	'song':song,
     'album_songs':album_songs,
-    'song_reviews':song_reviews
+    'song_reviews_users':song_reviews_users,
+    'song_reviews_critics':song_reviews_critics,
+    'review_user_rating_average_critic':review_user_rating_average_critic,
+    'review_user_rating_average_user':review_user_rating_average_user,
+	'review_user_rating_perfect':review_user_rating_perfect
 	}
 	)
 
